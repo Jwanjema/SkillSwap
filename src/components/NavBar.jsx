e20ce3d0a461fff9e6701f68ff2d374366271f01
Mohamed Abdulrashid <mohamedabdul9977@gmail.com>
2025-07-24 16:19:36 +0300

    Added home section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,9 @@ import './NavBar.css'
 export default function NavBar() {
   return (
     <nav className="navbar">
-      <Link to="/" className="logo"><img src='public/logo.png' alt='SkillSwap logo'></img></Link>
+      <Link to="/" className="logo"><img src='/logo.png' alt='SkillSwap logo'></img></Link>
       <div className="nav-links">
+        <Link to="/" className="nav-link">Home</Link>
         <Link to="/skills" className="nav-link">Browse</Link>
         <Link to="/add-skill" className="nav-link">Add Skill</Link>
         <Link to="/profile" className="nav-link">Profile</Link>
