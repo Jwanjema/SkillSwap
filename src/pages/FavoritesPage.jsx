df09bd2502323ff54e93251b1a2ca4c05c76cbbd
joewaweruwanjema <wawerujoe@gmail.com>
2025-07-24 10:58:15 +0300

    added favorites page

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import SkillList from '../components/SkillList';
+import './FavoritesPage.css';
 
+export default function FavoritesPage() {
+  const [favorites, setFavorites] = useState([]);
 
-function FavoritesPage() {}
+  useEffect(() => {
+    axios.get('http://localhost:3000/favorites')
+      .then(res => setFavorites(res.data))
+      .catch(err => console.error(err));
+  }, []);
 
-export default FavoritesPage;
+  return (
+    <div className="favorites-page">
+      <h2>Your Favorites</h2>
+      <SkillList skills={favorites} />
+    </div>
+  );
+}
